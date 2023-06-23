5f7b87c3f227172ffd24f218538cc5d7ad7108cc
JÂNIO FLORÊNCIO DOS SANTOS <info.janio@gmail.com>
2023-06-23 19:56:42 +0000

    lista produtos by subcategory

--- src/modules/products/useCases/lisAvailableProducts/ListAvailableController.ts
+++ src/modules/products/useCases/lisAvailableProducts/ListAvailableController.ts
@@ -4,15 +4,15 @@ import { ListAvailableProductsUseCase } from './ListAvailableProductsUseCase';
 
 class ListAvailableController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { brand, name, category_id } = request.query;
+    const { name, subcategory_id } = request.query;
 
     const listAvalableProductsUseCase = container.resolve(
       ListAvailableProductsUseCase,
     );
     const products = listAvalableProductsUseCase.execute({
-      brand: brand as string,
+     // brand: brand as string,
       name: name as string,
-      category_id: category_id as string,
+      subcategory_id: subcategory_id as string,
     });
     console.log('controller=', products);
     return response.json(products);
